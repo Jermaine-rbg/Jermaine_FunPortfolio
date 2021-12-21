b1d5193ed7a0bb30b78afe808a95d07c65a52df9
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2021-12-21 11:42:33 -0500

    About page update

--- constants/data.js
+++ constants/data.js
@@ -52,12 +52,12 @@ const userData = {
     title:
       "I'm a software developer that loves building products and web applications that's functional, stylish and practical",
     description: [
-      `I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
-      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
-      `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+      `I've been coding for a year and half now, self taught for a year. I didn't know what full-stack meant at that time until after doing intensive research.`,
+      `After learning HTML and struggling with CSS, heading into Javascript I finally attended Digital Crafts a top-rated Full Stack Bootcamp located here in Atlanta.`,
+      `During my time in bootcamp I learned the following tech stack, HTML CSS, JavaScript, React, Node.js, Express, and PostgresSQL folowing with self taught in Next.Js, Bootstrap, TailwindCSS, databases Firebase, MongoDb, SupaBase with acquiring some basic fundamamentals in AWS, Netlify and Varcel cloud deployment technology. I consider myself focus driven, eager to learn more and a well balanced team player.`,
     ],
-    currentProject: "Tailwind Master Kit",
-    currentProjectUrl: "https://tailwindmasterkit.com",
+    currentProject: "Front-End Development",
+    currentProjectUrl: "https://trio.dev/blog/front-end-technologies",
   },
   experience: [
     {
