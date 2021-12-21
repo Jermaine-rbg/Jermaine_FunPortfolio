5f8848e58fec6276746f3499dcbebe3ec8fbdb79
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2021-12-21 14:01:04 -0500

    Meta Commits

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -8,7 +8,7 @@ export default function ContainerBlock({ children, ...customMeta }) {
   const router = useRouter();
 
   const meta = {
-    title: "Jermaine Bailey - Developer, Writer, Creator and YouTuber",
+    title: "Jermaine Bailey - Software Developer",
     description: `I've been developing websites for 5 years straight. Get in touch with me to know more.`,
     image: "/avatar.png",
     type: "website",
@@ -29,7 +29,7 @@ export default function ContainerBlock({ children, ...customMeta }) {
           href={`https://yourwebsite.com${router.asPath}`}
         />
         <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Manu Arora" />
+        <meta property="og:site_name" content="Jermaine Bailey" />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
