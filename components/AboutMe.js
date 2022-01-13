9c5a14ef4cccd9635b01f25e2615fa2ccdaf6571
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-01-12 20:47:28 -0500

    Update AboutMe.js

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -74,10 +74,7 @@ export default function AboutMe() {
                   className="flex flex-row items-center space-x-4 group"
                 >
                   <div className="my-4">&rarr;</div>
-                  <p className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
-                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
-                    Twitter
-                  </p>
+                  
                 </a>
               </div>
               <div className="flex flex-row justify-start items-center">
